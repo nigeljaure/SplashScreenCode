662063773d72e9a90e9d4ec05b43f8e8a87e75ef
nigel jaure <outradev@gmail.com>
2017-10-09 13:18:52 +0200

    Splash screen code as recommended by Google design specifications

---
+++ java/nigeljaure/splashscreen/MainActivity.java
@@ -0,0 +1,16 @@
+package nigeljaure.splashscreen;
+/*
+  MainActivity will start when SplashScreen is launched
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
